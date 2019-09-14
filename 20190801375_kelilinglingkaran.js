08016c23ed274b3aab397fa224df7d52ccdc486f
Kresna Ramadhan <dhanns777@gmail.com>
2019-09-14 13:31:28 +0000

    Delete 20190801375_kelilinglingkaran.js

--- 20190801375_kelilinglingkaran.js
+++
@@ -1,5 +0,0 @@
-var infrom_keyboard = require('readline-sync');
-var pi = 3.14;
-var jari_jari = infrom_keyboard.question("Masukkan nilai r : ");
-var luas_lingkaran = pi * pi * jari_jari;
-console.log("Luas Lingkaran : "+luas_lingkaran);
